52d9e290e0b44201010014bbea8cca1ce65afe6c
Garret Browning <garret.browning@uj.edu>
2021-05-07 10:37:49 -0500

    Created AI Controller C++ class and blueprint

---
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,5 @@
+// Copyright Garret Browning 2021
+
+
+#include "ShooterAIController.h"
+
---
+++ Source/SimpleShooter/ShooterAIController.h
@@ -0,0 +1,17 @@
+// Copyright Garret Browning 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShooterAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API AShooterAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
